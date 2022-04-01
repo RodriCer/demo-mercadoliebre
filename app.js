c1889ac3529852c001db43cd453a294e7610abc6
RodriCer <rodri.cernuda@gmail.com>
2022-03-31 21:26:13 -0300

    Add heroku config

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));//Queremos a public como una carpeta de recursos estaticos
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en el puerto 3000");
-});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"views/home.html"));
